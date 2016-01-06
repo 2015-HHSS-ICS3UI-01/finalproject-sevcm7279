637fd91a62f5c004f8f9ca4b52ae4fcc8d0de2e0
sevcm7279 <sevcm7279@HRH-IC0027347.wrdsb.ca>
2016-01-06 14:24:50 -0500

    l

--- src/SquareInavaders.java
+++ src/SquareInavaders.java
@@ -1,6 +1,9 @@
 
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.ArrayList;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
 
@@ -13,7 +16,7 @@ import javax.swing.JFrame;
 public class SquareInavaders extends JComponent{
 
     // Height and Width of our game
-    static final int WIDTH = 800;
+    static final int WIDTH = 760;
     static final int HEIGHT = 600;
     
     // sets the framerate and delay for our game
@@ -21,7 +24,13 @@ public class SquareInavaders extends JComponent{
     long desiredFPS = 60;
     long desiredTime = (1000)/desiredFPS;
     
-
+    //create the user player
+    //start at the centre near the bottom of the screen
+    Rectangle player = new Rectangle(360, 520, 40, 40);
+    
+    
+    //create rectangle array liost for the enemy squares
+    ArrayList<Rectangle> blocks = new ArrayList<>();
     
     // drawing of the game happens in here
     // we use the Graphics object, g, to perform the drawing
@@ -34,6 +43,13 @@ public class SquareInavaders extends JComponent{
         
         // GAME DRAWING GOES HERE 
         
+        for(Rectangle block: blocks){
+            g.fillRect(block.x, block.y, block.width, block.height);
+        }
+        //put player on screen
+        //make the player blue
+        g.setColor(Color.BLUE);
+        g.fillRect(player.x, player.y, player.width, player.height);
         
         // GAME DRAWING ENDS HERE
     }
@@ -43,6 +59,33 @@ public class SquareInavaders extends JComponent{
     // In here is where all the logic for my game will go
     public void run()
     {
+        
+        //add enemy squares in three rows
+        //start at the top of the screen
+        
+        //first row
+        blocks.add(new Rectangle(30, 0, 40, 40));
+        blocks.add(new Rectangle(140, 0, 40, 40));
+        blocks.add(new Rectangle(250, 0, 40, 40));
+        blocks.add(new Rectangle(360, 0, 40, 40));
+        blocks.add(new Rectangle(470, 0, 40, 40));
+        blocks.add(new Rectangle(580, 0, 40, 40));
+        blocks.add(new Rectangle(690, 0, 40, 40));
+        
+        //second row
+        blocks.add(new Rectangle(85, 50, 40, 40));
+        blocks.add(new Rectangle(195, 50, 40, 40));
+        blocks.add(new Rectangle(305, 50, 40, 40));
+        blocks.add(new Rectangle(415, 50, 40, 40));
+        blocks.add(new Rectangle(525, 50, 40, 40));
+        blocks.add(new Rectangle(635, 50, 40, 40));
+        
+        //third row
+        blocks.add(new Rectangle(140, 100, 40, 40));
+        blocks.add(new Rectangle(250, 100, 40, 40));
+        blocks.add(new Rectangle(360, 100, 40, 40));
+        blocks.add(new Rectangle(470, 100, 40, 40));
+        blocks.add(new Rectangle(580, 100, 40, 40));
         // Used to keep track of time used to draw and update the game
         // This is used to limit the framerate later on
         long startTime;
